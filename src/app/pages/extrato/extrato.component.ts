dee1f594a9d8adea8704142b700721f87d0c9f8d
Paulo Jose Rocha Dantas <paulo.dantas@srmasset.com>
2019-02-19 16:30:53 -0300

    acerto de serviço quebrado

--- src/app/pages/extrato/extrato.component.ts
+++ src/app/pages/extrato/extrato.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ExtratoService } from '../../services/extrato.service';
+import { Extrato } from '../../services/extrato';
 
 @Component({
   selector: 'app-extrato',
@@ -8,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
-  constructor() { }
+  private extratos: Extrato[] = new Array();
+
+  constructor(private extratoService: ExtratoService, private router: Router) { }
 
   ngOnInit() {
+
+    this.extratoService.getExtratos().subscribe(res => this.extratos = res);
   }
 
 }
